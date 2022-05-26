6818b816535130d5d45a209b89a0a2c4212cb3e9
doccreeps <dorian1998dino@hotmail.com>
2022-05-26 22:35:21 +0200

    card projet

--- resources/views/Component/About.blade.php
+++ resources/views/Component/About.blade.php
@@ -5,6 +5,6 @@
         class="rounded-full w-32 mb-4 mx-auto"
         alt="Avatar"
     />
-    <p class="text-dark-500">Je m'appel Dorian Vericel je suis actuellement étudiant en MASTER ESI(Expert en Services d'Informations) au sein de l'école ISITECH et en Alternance en tant que développeur/webmaster et responsable de projet ERP chez Skyepharma !</p>
+    <p class="text-dark-500 text-xl">Je m'appel Dorian Vericel je suis actuellement étudiant en MASTER ESI(Expert en Services d'Informations) au sein de l'école ISITECH et en Alternance en tant que développeur/webmaster et responsable de projet ERP chez Skyepharma !</p>
 </div>
 </section>
--- resources/views/Component/Projet.blade.php
+++ resources/views/Component/Projet.blade.php
@@ -1,7 +1,9 @@
 
 <section>
-    <h1 class="text-5xl text-center text-gray-700 font-bold mt-20 mb-20 -ml-3">Projets</h1>
-    <div class="flex justify-center">
+    <h1 class="text-5xl text-center text-gray-700 font-bold mt-20 mb-20 ">Projets</h1>
+    <div class="text-center items-center flex justify-around">
+
+<div class="">
         <div class="rounded-lg shadow-lg bg-white max-w-sm">
             <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
                 <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/182.jpg" alt=""/>
@@ -15,6 +17,60 @@
                 <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
             </div>
         </div>
+</div>
+    <div class="">
+        <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/182.jpg" alt=""/>
+            </a>
+            <div class="p-6">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    Some quick example text to build on the card title and make up the bulk of the card's
+                    content.
+                </p>
+                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+
+    <div class="text-center items-center flex justify-around mt-20 mb-20">
+
+        <div class="">
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                    <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/182.jpg" alt=""/>
+                </a>
+                <div class="p-6">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                    <p class="text-gray-700 text-base mb-4">
+                        Some quick example text to build on the card title and make up the bulk of the card's
+                        content.
+                    </p>
+                    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                </div>
+            </div>
+        </div>
+        <div class="">
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                    <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/182.jpg" alt=""/>
+                </a>
+                <div class="p-6">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                    <p class="text-gray-700 text-base mb-4">
+                        Some quick example text to build on the card title and make up the bulk of the card's
+                        content.
+                    </p>
+                    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 
 </section>
