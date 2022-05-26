9208c479f6bac6f49b8ac8f7aac0eb072498e5ed
doccreeps <dorian1998dino@hotmail.com>
2022-05-26 16:26:07 +0200

    section experience remplis design card a modifier pas toute meme taille

--- resources/views/Component/Experience.blade.php
+++ resources/views/Component/Experience.blade.php
@@ -14,7 +14,13 @@
                             <a class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">02/2021-02/2023</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Alternant Développeur, Webmaster et Responsable ERP</h3>
-                        <p class="text-gray-700 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula.</p>
+                        <p class="text-gray-700 mb-6">Alternance effectuer dans le cadre de ma formation Expert en Systèmes d'Informations<br/>
+                            Missions :<br/>
+                            Développement d'applications(Maintenance d'application métier (en windev), création d'une web application de gestion des temps projets en Laravel),<br/>
+                            WebMaster(Mainteance et amélioration du site sous Wordpress, Amélioration du score SEO).<br/>
+                            Responsable de projet ERP Sage x3,<br/></p>
+
+
                     </div>
                 </div>
             </li>
@@ -29,7 +35,12 @@
                             <a class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">09/2019-10/2020</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Alternant Développeur</h3>
-                        <p class="text-gray-700 mb-6">Libero expedita explicabo eius fugiat quia aspernatur autem laudantium error architecto recusandae natus sapiente sit nam eaque, consectetur porro molestiae ipsam an deleniti.</p>
+                        <p class="text-gray-700 mb-6">Alternance dans le cadre de la licence RPI(Responsable de Projet Informatique)<br/>
+                            Missions :<br/>
+                            Maintenance d'application web, Conception d'application web,<br/>
+                            Utilisation de (PHP,JS,HTML,CSS,SQL),<br/>
+                            Framework PHP Maison,<br/>
+                            Librairies : Datatable</p>
                     </div>
                 </div>
             </li>
@@ -43,7 +54,8 @@
                             <a class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">01/2019-02/2019</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Stage - Développeur WEB </h3>
-                        <p class="text-gray-700 mb-6">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <p class="text-gray-700 mb-6">Création d'une page d'accueil pour un outils de vidéo conférence avec le framework LARAVEL<br/>
+                            Etude de l'ERP ODOO</p>
                     </div>
                 </div>
             </li>
@@ -57,7 +69,9 @@
                             <a class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">05/2018-06/2018</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Stage - Développeur</h3>
-                        <p class="text-gray-700 mb-6">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <p class="text-gray-700 mb-6">Modification du code du site WEB<br/>
+                            Conception de requêtes VBA<br/>
+                            Maintenance Informatique </p>
                     </div>
                 </div>
             </li>
@@ -71,7 +85,8 @@
                             <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">01/2017-02/2017</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Stage - Maintenance informatique</h3>
-                        <p class="text-gray-700 mb-6">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <p class="text-gray-700 mb-6">Utilisation de GLPI pour faire un inventaire du parc informatique<br/>
+                            Maintenance informatique</p>
                     </div>
                 </div>
             </li>
@@ -85,7 +100,7 @@
                             <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">04/2016-05/2016</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Stage - Maintenance informatique</h3>
-                        <p class="text-gray-700 mb-6">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <p class="text-gray-700 mb-6">Stage maintenance informatique</p>
                     </div>
                 </div>
             </li>
@@ -99,7 +114,7 @@
                             <a class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">11/2015</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Stage - Maintenance informatique</h3>
-                        <p class="text-gray-700 mb-6">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <p class="text-gray-700 mb-6">Stage maintenance informatique</p>
                     </div>
                 </div>
             </li>
@@ -113,7 +128,7 @@
                             <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">05/2015</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Stage - Maintenance informatique</h3>
-                        <p class="text-gray-700 mb-6">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <p class="text-gray-700 mb-6">Stage maintenance informatique</p>
                     </div>
                 </div>
             </li>
@@ -127,7 +142,7 @@
                             <a href="#!" class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">02/2015-03/2015</a>
                         </div>
                         <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Stage - Maintenance informatique</h3>
-                        <p class="text-gray-700 mb-6">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+                        <p class="text-gray-700 mb-6">Stage maintenance informatique</p>
                     </div>
                 </div>
             </li>
