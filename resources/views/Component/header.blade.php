96d5385b0c7ecb084ec16746d1cb2807b5820c1b
doccreeps <dorian1998dino@hotmail.com>
2022-05-31 15:02:12 +0200

    animation text reste cursor a faire

--- resources/views/Component/header.blade.php
+++ resources/views/Component/header.blade.php
@@ -92,8 +92,51 @@
     <div class="container mx-auto px-6 md:px-12 xl:px-30">
         <div class="text-center text-gray-800">
             <div class="block rounded-lg shadow-lg px-6 py-12 md:py-16 md:px-12" style="margin-top: -170px; background: hsla(0, 0%, 100%, 0.7); backdrop-filter: blur(30px);">
-                <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Portfolio Dorian VERICEL <br /><span class="text-blue-600">Développeur</span></h1>
+                <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Portfolio Dorian VERICEL <br /><span class="text-blue-600" id="typed-text"></span><span class="cursor">&nbsp;</span></h1>
             </div>
         </div>
     </div>
 </header>
+
+<script>
+    const typedTextSpan = document.querySelector("#typed-text");
+    const cursorSpan = document.querySelector(".cursor");
+
+    const textArray = ["Développeur Web", "Développeur Full Stack"];
+    const typingDelay = 150;
+    const erasingDelay = 125;
+    const newTextDelay = 2000; // Delay between current and next text
+    let textArrayIndex = 0;
+    let charIndex = 0;
+    function type() {
+        if (charIndex < textArray[textArrayIndex].length) {
+            if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        }
+        else {
+            cursorSpan.classList.remove("typing");
+            setTimeout(erase, newTextDelay);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        }
+        else {
+            cursorSpan.classList.remove("typing");
+            textArrayIndex++;
+            if(textArrayIndex>=textArray.length) textArrayIndex=0;
+            setTimeout(type, typingDelay + 1100);
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+        if(textArray.length) setTimeout(type, newTextDelay + 250);
+    });
+</script>
