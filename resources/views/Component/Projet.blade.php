0ae63d9bf487fae7596a22a427b3eb54c7bb438c
DocCreeps <dorian1998dino@hotmail.com>
2022-06-06 14:31:36 +0200

    projet responsive

--- resources/views/Component/Projet.blade.php
+++ resources/views/Component/Projet.blade.php
@@ -4,114 +4,192 @@
 
     <h3 class="text-3xl text-center text-gray-700 font-bold mt-20 mb-20 -ml-3 hover:underline" id="ProjetPerso">Projets Perso</h3>
 
-    <div class="text-center items-center flex justify-around">
 
-<div class="">
-        <div class="rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-full mx-auto hover:animate-pulse" src="{{asset('Picture/Projets/logosrv.jpg')}}" alt="logo projet Arkanda" title="Logo Arkanda"/>
-            </a>
-            <div class="p-6">
-                <h4 class="text-gray-900 text-xl font-medium mb-2">Arkanda</h4>
-                <p class="text-gray-700 text-base mb-4">
-                    Projet en cours 🛠️<br/> Projet avec des amis qui à pour but la création d'un serveur mini jeux sur le jeu minecraft.
-                </p>
-{{--                <iframe src="https://discord.com/widget?id=569957594794426378&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe><br/>--}}
-                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Le Projet</button>
+    <div class="container my-12 mx-auto px-4 md:px-12">
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+
+            <!-- Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
+
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="https://github.com/DocCreeps/roulette" target="_blank" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                        <img class="rounded-full mx-auto hover:animate-pulse" src="{{asset('Picture/Projets/logosrv.jpg')}}" alt="logo projet Arkanda" title="Logo Arkanda"/>
+                    </a>
+
+                    <header class="flex items-center text-center justify-between leading-tight p-2 md:p-4">
+                        <h4 class="text-gray-900 text-xl font-medium mb-2 mx-auto">Arkanda</h4>
+                    </header>
+
+                    <footer class=" items-center justify-between text-center leading-none mb-5">
+                        <p class="text-gray-700 text-base mb-4">
+                            Projet en cours 🛠️<br/> Projet avec des amis qui à pour but la création d'un serveur mini jeux sur le jeu minecraft.
+                        </p>
+                        {{--                <iframe src="https://discord.com/widget?id=569957594794426378&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe><br/>--}}
+                        <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Le Projet</button>
+
+                    </footer>
+
+                </article>
+                <!-- END Article -->
+
             </div>
-        </div>
-</div>
-    <div class="">
-        <div class="rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/gestpass.png')}}" alt="logo projet Gest Pass"   title="Logo Gest Pass"/>
-            </a>
-            <div class="p-6">
-                <h4 class="text-gray-900 text-xl font-medium mb-2">Gest Pass</h4>
-                <p class="text-gray-700 text-base mb-4">
-                    Projet en cours 🛠️<br/> Création d'un gestionnaire de mot de passe. <br/>
-                    techno : <br/>
-                    <b>Laravel, Tailwind, Mariadb</b>
-                </p>
-                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="http://gestpass.dorianvericel.fr/">Demo</a></button>
+            <!-- END Column -->
+            <!-- Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
+
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="https://github.com/DocCreeps/roulette" target="_blank" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                        <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/gestpass.png')}}" alt="logo projet Gest Pass"   title="Logo Gest Pass"/>
+                    </a>
+
+                    <header class="flex items-center text-center justify-between leading-tight p-2 md:p-4">
+                        <h4 class="text-gray-900 text-xl font-medium mb-2 mx-auto">Gest Pass</h4>
+                    </header>
+
+                    <footer class=" items-center justify-between text-center leading-none mb-5">
+                        <p class="text-gray-700 text-base mb-4">
+                            Projet en cours 🛠️<br/> Création d'un gestionnaire de mot de passe. <br/>
+                            techno : <br/>
+                            <b>Laravel, Tailwind, Mariadb</b>
+                        </p>
+                        <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="http://gestpass.dorianvericel.fr/">Demo</a></button>
+                    </footer>
+
+                </article>
+                <!-- END Article -->
+
             </div>
-        </div>
-    </div>
+            <!-- END Column -->
+            <!-- Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
 
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg shadow-lg">
 
-    </div>
+                    <a href="https://github.com/DocCreeps/roulette" target="_blank" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                        <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/Blog.png')}}" alt="logo projet Blog" title="Logo Blog"/>
+                    </a>
 
-    <div class="text-center items-center flex justify-around mt-20 mb-20">
+                    <header class="flex items-center text-center justify-between leading-tight p-2 md:p-4">
+                        <h4 class="text-gray-900 text-xl font-medium mb-2 mx-auto">Blog</h4>
+                    </header>
 
-        <div class="">
-            <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                    <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/Blog.png')}}" alt="logo projet Blog" title="Logo Blog"/>
-                </a>
-                <div class="p-6">
-                    <h4 class="text-gray-900 text-xl font-medium mb-2">Blog</h4>
-                    <p class="text-gray-700 text-base mb-4">
-                        Projet en cours 🛠️<br/> Création d'un blog permettant la création et consultation d'article.<br/>
-                        techno : <br/>
-                        <b>Laravel, Tailwind, Mariadb</b>
-                    </p>
-                    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="http://blog.dorianvericel.fr/">Le Projet</a></button>
-                </div>
+                    <footer class=" items-center justify-between text-center leading-none mb-5">
+                        <p class="text-gray-700 text-base mb-4">
+                            Projet en cours 🛠️<br/> Création d'un blog permettant la création et consultation d'article.<br/>
+                            techno : <br/>
+                            <b>Laravel, Tailwind, Mariadb</b>
+                        </p>
+                        <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="http://blog.dorianvericel.fr/">Le Projet</a></button>
+                    </footer>
+
+                </article>
+                <!-- END Article -->
+
             </div>
-        </div>
-        <div class="">
-            <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                    <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/Cyberveillepng.png')}}" alt="logo projet cyber veille" title="Logo Cyber Veille"/>
-                </a>
-                <div class="p-6">
-                    <h4 class="text-gray-900 text-xl font-medium mb-2">Cyber Veille</h4>
-                    <p class="text-gray-700 text-base mb-4">
-                        Projet en cours 🛠️<br/>Application web permettant de faire une veille technologique et de classifier les articles. <br/>
-                        techno : <br/>
-                        <b>Laravel, Tailwind, Mariadb</b>
-                    </p>
-                    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="http://cyberveille.dorianvericel.fr/">Le Projet</a></button>
-                </div>
+            <!-- END Column -->
+            <!-- Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
+
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="https://github.com/DocCreeps/roulette" target="_blank" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                        <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/Cyberveillepng.png')}}" alt="logo projet cyber veille" title="Logo Cyber Veille"/>
+                    </a>
+
+                    <header class="flex items-center text-center justify-between leading-tight p-2 md:p-4">
+                        <h4 class="text-gray-900 text-xl font-medium mb-2 mx-auto">Cyber Veille</h4>
+                    </header>
+
+                    <footer class=" items-center justify-between text-center leading-none mb-5">
+                        <p class="text-gray-700 text-base mb-4">
+                            Projet en cours 🛠️<br/> Création d'un blog permettant la création et consultation d'article.<br/>
+                            techno : <br/>
+                            <b>Laravel, Tailwind, Mariadb</b>
+                        </p>
+                        <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"><a href="http://blog.dorianvericel.fr/">Le Projet</a></button>
+
+                    </footer>
+
+                </article>
+                <!-- END Article -->
+
             </div>
+            <!-- END Column -->
         </div>
     </div>
 
     <h3 class="text-3xl text-center text-gray-700 font-bold mt-20 mb-20 -ml-3 hover:underline" id="ProjetScolaire">Projets Scolaire</h3>
 
     <h4 class="text-2xl text-center text-gray-700 font-bold mt-20 mb-20 -ml-3 hover:underline" id="ProjetMaster">Projets Licence et Master</h4>
-    <div class="text-center items-center flex justify-around mt-20 mb-20">
 
-        <div class="">
-            <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                <a href="#"  target="_blank" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                    <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/.jpg')}}" alt="logo projet isidrive" title="Logo isidrive"/>
-                </a>
-                <div class="p-6">
-                    <h4 class="text-gray-900 text-xl font-medium mb-2">IsiDrive</h4>
-                    <p class="text-gray-700 text-base mb-4">
-                        Application web et mobile de gestion d'un drive.<br/>
-                        techno : <br/>
-                       <b>PHP, JS, C#, SQL SERVER</b>
-                    </p>
-                </div>
+    <div class="container my-12 mx-auto px-4 md:px-12">
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+
+            <!-- Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
+
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="https://github.com/DocCreeps/roulette" target="_blank" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                        <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/isidrive.jpg')}}" alt="logo projet isidrive" title="Logo isidrive"/>
+                    </a>
+
+                    <header class="flex items-center text-center justify-between leading-tight p-2 md:p-4">
+                        <h4 class="text-gray-900 text-xl font-medium mb-2 mx-auto">IsiDrive</h4>
+                    </header>
+
+                    <footer class=" items-center justify-between text-center leading-none">
+                        <p class="text-gray-700 text-base mb-4">
+                            Application web et mobile de gestion d'un drive.<br/>
+                            techno : <br/>
+                            <b>PHP, JS, C#, SQL SERVER</b>
+                        </p>
+                    </footer>
+
+                </article>
+                <!-- END Article -->
+
             </div>
-        </div>
-        <div class="">
-            <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                    <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/zjd.png')}}" alt="logo projet zjd" title="Logo ZJD"/>
-                <div class="p-6">
-                    <h4 class="text-gray-900 text-xl font-medium mb-2">ZJD Virtualisation</h4>
-                    <p class="text-gray-700 text-base mb-4">
-                        Projet en groupes durant mon année de Licence RPI  <br/>
-                        Création d'une entreprise qui propose une solution de virtualisation avec docker. <br/>
-                        techno : <br/>
-                        <b>PHP(symphony), JS, dockers, mariadb</b>
-                    </p>
-                </div>
+            <!-- END Column -->
+            <!-- Column -->
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
+
+                <!-- Article -->
+                <article class="overflow-hidden rounded-lg shadow-lg">
+
+                    <a href="https://github.com/DocCreeps/roulette" target="_blank" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                        <img class="rounded-t-lg mx-auto" src="{{asset('Picture/Projets/zjd.png')}}" alt="logo projet zjd" title="Logo ZJD"/>
+                    </a>
+
+                    <header class="flex items-center text-center justify-between leading-tight p-2 md:p-4">
+                        <h4 class="text-gray-900 text-xl font-medium mb-2 mx-auto">ZJD Virtualisation</h4>
+                    </header>
+
+                    <footer class=" items-center justify-between text-center leading-none">
+                        <p class="text-gray-700 text-base mb-4">
+                            Projet en groupes durant mon année de Licence RPI  <br/>
+                            Création d'une entreprise qui propose une solution de virtualisation avec docker. <br/>
+                            techno : <br/>
+                            <b>PHP(symphony), JS, dockers, mariadb</b>
+                        </p>
+                    </footer>
+
+                </article>
+                <!-- END Article -->
+
             </div>
+            <!-- END Column -->
         </div>
     </div>
+
+
 
     <h4 class="text-2xl text-center text-gray-700 font-bold mt-20 mb-20 -ml-3 hover:underline" id="projetBts">Projets BTS</h4>
 
