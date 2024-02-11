1413e174aba027c3a50774e1a97fad42a60e9242
DocCreeps <dorian1998dino@hotmail.com>
2024-02-11 11:01:36 +0100

    correction orthographe formation .

--- resources/views/Component/Formation.blade.php
+++ resources/views/Component/Formation.blade.php
@@ -11,7 +11,7 @@
                     <p  class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">2019/2023</p>
                 </div>
                 <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> Cycle d'ingénierie Expert en Systèmes dIinformations </h3>
-                <p class="text-gray-700 mb-2 ">J'ai effectué le cycle d'ingénierie ESI<br/>Option développeur au seins de l'école Isitech en alternance.</p>
+                <p class="text-gray-700 mb-2 ">J'ai effectué le cycle d'ingénierie ESI<br/>Option développeur au sein de l'école Isitech en alternance.</p>
 
             </div>
             <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
@@ -23,47 +23,7 @@
                 ></div>
             </div>
         </div>
-        <!-- right -->
-     {{--   <div class="flex md:contents">
 
-            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-                <div class="h-full w-6 flex items-center justify-center">
-                    <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
-                </div>
-                <div
-                    class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-400 shadow"
-                ></div>
-            </div>
-            <div class="bg-gray-100 text-center col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-                <div class="flex flex-wrap justify-between mb-4">
-                    <p class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">CNIL</p>
-                    <p class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">08/2019</p>
-                </div>
-                <h2 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> MOOC RGPD</h2>
-                <p class="text-gray-700 mb-2">Mooc sur les bonnes pratiques RGPD .</p>
-            </div>
-        </div>
-        <!-- left -->
-        <div class="flex flex-row-reverse md:contents">
-            <div
-                class="bg-gray-100 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md text-center">
-                <div class="flex flex-wrap justify-between mb-4">
-                    <p  class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">ANSSI</p>
-                    <p class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">02/2019</p>
-                </div>
-                <h2 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> MOOC Sec Num Académie</h2>
-                <p class="text-gray-700 mb-2">Mooc d'initiation à la cybersécurité.</p>
-            </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-                <div class="h-full w-6 flex items-center justify-center">
-                    <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
-                </div>
-                <div
-                    class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-400 shadow"
-                ></div>
-            </div>
-        </div>--}}
-        <!-- right -->
         <div class="flex md:contents">
 
             <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
@@ -81,7 +41,7 @@
                     <p  class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm">2017/2019</p>
                 </div>
                 <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> BTS SIO option SLAM</h3>
-                <p class="text-gray-700 mb-2">J'ai effectuer mon BTS SIO (Services Informatiques aux Organisations) <br/>Option SLAM(Solution Logiciels et Applications Metiers) <br/>au Centre Scolaire Notre Dame a Villefranche-sur-Saône.<br/> Avec ce BTS j'ai pu apprendre les bases de la programmations  </p>
+                <p class="text-gray-700 mb-2">J'ai effectué mon BTS SIO (Services Informatiques aux Organisations) <br/>Option SLAM (Solution Logiciels et Applications Metiers) <br/>au Centre Scolaire Notre Dame à Villefranche-sur-Saône.<br/> Avec ce BTS j'ai pu apprendre les bases de la programmation . </p>
 
             </div>
         </div>
@@ -90,12 +50,12 @@
             <div class="bg-gray-100 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md text-center formationReveal">
 
                 <div class="flex flex-wrap justify-between mb-4">
-                    <p  class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm ">Le marais sainte thérèse</p>
+                    <p  class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm ">Le Marais Sainte Thérèse</p>
                     <p  class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm ">Saint-Étienne</p>
                     <p class="font-medium text-purple-600 hover:text-purple-700 focus:text-purple-800 duration-300 transition ease-in-out text-sm ">2014/2017</p>
                 </div>
                 <h3 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-600"> BEP et BAC Pro SEN option TR</h3>
-                <p class="text-gray-700 mb-2 overflow-auto">J'ai effectuer mon BEP et mon BAC Professionnel SEN(Système Électronique et Numérique) <br/>option TR (Telecom et Réseaux) au sein du lycée Le marais sainte thérèse a Saint-Étienne.<br/> Ce BAC Pro m'a appris les bases de l'informatique et de l'électronique </p>
+                <p class="text-gray-700 mb-2 overflow-auto">J'ai effectué mon BEP et mon BAC Professionnel SEN (Système Électronique et Numérique) <br/>option TR (Telecom et Réseaux) au sein du lycée Le Marais Sainte Thérèse à Saint-Étienne.<br/> Ce BAC Pro m'a appris les bases de l'informatique et de l'électronique .</p>
 
 
             </div>
